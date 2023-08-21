b236a6285461457a6fc46392f09b8189b585f7de
Fabrice Armisen <farmisen@gmail.com>
2023-08-20 19:48:58 -0700

    simple astro app displaying a react component

---
+++ .prettierrc.cjs
@@ -0,0 +1,22 @@
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+  printWidth: 88,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
